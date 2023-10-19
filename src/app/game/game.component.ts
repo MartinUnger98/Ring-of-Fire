b5080479728d97d71d068e6b319e833b97f8272b
Martin Unger <126020447+MartinUnger98@users.noreply.github.com>
2023-10-19 11:01:39 +0000

    new game + takeCard()

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  pickCardAnnimation = false;
+  currentCard: string ='';
+  game: Game;
 
+
+  constructor() {
+    this.game = new Game();
+    this.currentCard = '';
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    let poppedCard = this.game.stack.pop();
+    if (poppedCard !== undefined && !this.pickCardAnnimation) {
+      this.currentCard = poppedCard;
+      this.pickCardAnnimation = true;
+      
+      setTimeout(() => {
+        this.pickCardAnnimation = false
+      }, 1500);
+    }
+  }
 }
---
+++ src/models/game.ts
@@ -0,0 +1,29 @@
+export class Game {
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0;
+
+    constructor() {
+        for (let i = 1; i < 14; i++) {
+            this.stack.push('ace_' + i);   
+            this.stack.push('clubs_' + i);   
+            this.stack.push('diamonds_' + i);   
+            this.stack.push('hearts_' + i);   
+        }
+        shuffle(this.stack);
+        
+    }
+}
+
+function shuffle(array: any[]) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
